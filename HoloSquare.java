dcd0c7cba25c2903cf1ed27a13742a6c53be8284
Shubham Bhatt <bhattg936@gmail.com>
2024-09-27 19:14:18 +0530

    add holosquare file

---
+++ HoloSquare.java
@@ -0,0 +1,35 @@
+public class HoloSquare{
+    public static void main(String [] args){
+        for(int i=1; i<=4;i++){
+            for (int j=1;j<=5;j++){
+                if(i<4&&i>1&&j<5&&j>1)
+                System.out.print(" ");
+                else
+                 System.out.print("*");
+            }
+            System.out.print("\n");
+        }
+    }
+}
+
+// ANOTHER WAY TO PRINT HOLO SQUARE USING STAR.
+/* 
+public class HoloSquare{
+    public static void main(String [] args){
+        for(int i=1; i<=4;i++){
+            for (int j=1;j<=5;j++){
+                if(i==1 || i==4 || j==5 || j==1)
+                System.out.print("*");
+                else
+                 System.out.print(" ");
+            }
+            System.out.print("\n");
+        }
+    }
+}
+the pattern is:
+*****
+*   *
+*   *
+*****
+*/
